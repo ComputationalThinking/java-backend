6c4def3dba5c49d415926415334a84391bfe8226
cheerddd <2664330029@qq.com>
2021-03-18 22:15:41 +0800

    News表操作

--- src/main/java/com/example/project_/markerhub/controller/NewsController.java
+++ src/main/java/com/example/project_/markerhub/controller/NewsController.java
@@ -12,17 +12,52 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 
 @RestController
+
+@RestController
 @RequestMapping("/News")
-public class NewsController {
-
+public class MemberController {
     @Autowired
-    NewsService newsService;
-
-    @GetMapping("/information")
-    public List<News> getData(){
-        List<News> list = newsService.findAll();
+    MemberService userService;
+    @GetMapping("/newsFindAll")
+    public List<News> findAll(){
+        List<News> list =userService.findAll();
         return list;
     }
-
+    @GetMapping("/newsFindByCondition")
+    public List<Object> findByCondition(){
+        List<Object> list =userService.findByCondition("identity","1");
+        return list;
+    }
+    @GetMapping("/newsUpdate")
+    public void update(){
+        News news=new News();
+        news.setId(1);
+        news.setTitle("The history of News");
+        news.setAuthor("水冰月");
+        news.setTime("2021-03-18");
+        news.setImg_boolean("1");
+        news.setImg_src("c:\desktop\op");
+        news.setContent("however");
+        news.setSort("2");
+        userService.update(news);
+    }
+    @GetMapping("/newsDelete")
+    public void delete(){
+        News news=new News();
+        news.setId(2);
+        userService.delete(news);
+    }
+    @GetMapping("/newsInsert")
+    public void insert(){
+        News news=new News();
+        news.setId(3);
+        news.setTitle("Hello World!");
+        news.setAuthor("happy monkey");
+        news.setTime("1996-03-18");
+        news.setImg_boolean("1");
+        news.setImg_src("main.jpg");
+        news.setContent("whatever");
+        news.setSort("3");
+        userService.insert(news);
+    }
 }
-
--- src/main/java/com/example/project_/markerhub/service/NewsService.java
+++ src/main/java/com/example/project_/markerhub/service/NewsService.java
@@ -6,16 +6,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public interface NewsService extends IService<News> {
-//    查询所有
+public interface MewsrService extends IService<News> {
     List<News> findAll();
-//    查询单个
-//    List<Object> findByCondition(String attribute,String key);
-//    添加数据
-//    void insert();
-//    修改数据
-//    void update(String attribute,String key,Integer id);
-//    删除数据
-//    void delete(Integer id);
+    //查询单个
+    List<News> findByCondition(String attribute,String key);
+    //    添加数据
+   void insert(News news);
+   //    修改数据
+   void update(News news);
+   //    删除数据
+   void delete(News news);
 }
